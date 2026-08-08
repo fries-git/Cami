dcdf93148185b6212a61e39b2ab48248d07a8051
fries <fries_09@hotmail.com>
2026-08-08 10:55:02 -0400

    Change password hashing from SHA-256 to SHA-512

--- hashdef.py
+++ hashdef.py
@@ -1,6 +1,6 @@
 import hashlib
 
 def hash(password):
-    return(hashlib.sha256(password.encode()).hexdigest())
+    return(hashlib.sha512(password.encode()).hexdigest())
 
 # This doesn't need to be a seperate file anymore it just used to use a different type that was bulky and bad so I made a seperate thing. Not gonna bother changing it.
