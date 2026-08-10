428ed48bba725d0080f208e70a7aee361e0dff4f
fries-git <fries_09@hotmail.com>
2026-08-10 02:47:55 -0400

    fix init.py for now

--- init.py
+++ init.py
@@ -2,11 +2,8 @@ import subprocess
 import sys
 
 http = subprocess.Popen([sys.executable, "servers/httpserver.py"])
-websocket = subprocess.Popen([sys.executable, "servers/websocketserver.py"])
 
 try:
     http.wait()
-    websocket.wait()
 except KeyboardInterrupt:
     http.terminate()
-    websocket.terminate()
