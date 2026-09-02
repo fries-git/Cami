6b1bea20b409912a4c9ae55e81eef2f2133fd482
fries-git <fries_09@hotmail.com>
2026-09-02 11:11:01 -0400

    missing files fix

--- servers/websocketserver.py
+++ servers/websocketserver.py
@@ -20,6 +20,12 @@ token = os.getenv("hooktoken")
 channels = {}
 passwords = {}
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+os.makedirs(os.path.join(BASE_DIR, "channels"), exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR, "dbs", "userdata"), exist_ok=True)
+os.makedirs(os.path.join(BASE_DIR, "uploads", "imagestorage"), exist_ok=True)
+
 async def webhookpost(body, username):
     avatar = f"https://cami.barfpile.dev/userpfp/{username}"
     data = {
